7a6ab6ae6c0e37900df807375a8665e4973cd0de
tacohed393 <valverine393@gmail.com>
2025-12-24 03:27:54 +0300

    Вроде победил долгую загрузку докера, поставил, но не настроил миграции, это ужас

--- app/models/place.py
+++ app/models/place.py
@@ -11,8 +11,8 @@ class Place(Base):
     city: Mapped[str] = mapped_column(String, index=True) 
     type: Mapped[str] = mapped_column(String)  
 
-    lat: Mapped[float] = mapped_column(Float, nullable=True)
-    lon: Mapped[float] = mapped_column(Float, nullable=True)
+    #lat: Mapped[float] = mapped_column(Float, nullable=True)
+    #lon: Mapped[float] = mapped_column(Float, nullable=True)
 
     price: Mapped[str] = mapped_column(String, nullable=True) 
 
